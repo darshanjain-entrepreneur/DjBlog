aa3db4189b48a3679d58d643b07499265240e40c
Darshan <darshanjain.godha@gmail.com>
2023-06-26 14:25:23 +0530

    implemented the connection between the user and blogs throught id

--- djBlog/controllers/blogController.js
+++ djBlog/controllers/blogController.js
@@ -1,4 +1,8 @@
+
+const { default: mongoose } = require("mongoose");
 const blogModel = require("../models/blogModel");
+const userModel = require("../models/userModel");
+
 
 
 
@@ -38,25 +42,37 @@ return res.status(200).send({
 exports.createBlogController = async (req , res) => {
 
 try {
-    
-    const {title , description , image} = req.body;
-    if(!title|| !description || !image ){
-
-        return res.status(400).send({
-          success:false,
-          message:"Fill all fields"
-        })
+    const { title, description, image, user } = req.body;
+    //validation
+    if (!title || !description || !image || !user) {
+      return res.status(400).send({
+        success: false,
+        message: "Please Provide ALl Fields",
+      });
+    }
+    const exisitingUser = await userModel.findById(user);
+    //validaton
+    if (!exisitingUser) {
+      return res.status(404).send({
+        success: false,
+        message: "unable to find user",
+      });
     }
 
-const newBlog = new blogModel({title , description , image});
-await newBlog.save();
-
-return res.status(201).send({
-    success:true,
-    message:"Blog created successfully",
-    newBlog
-})
-
+    const newBlog = new blogModel({ title, description, image, user });
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    await newBlog.save({ session });
+    exisitingUser.blogs.push(newBlog);
+    await exisitingUser.save({ session });
+    await session.commitTransaction();
+    await newBlog.save();
+    return res.status(201).send({
+      success: true,
+      message: "Blog Created!",
+      newBlog,
+    });   
+ 
 } catch (error) {
     console.log(error);
     return res.status(404).send({
@@ -138,12 +154,18 @@ exports.deleteBlogController = async (req , res) => {
     
 try {
 
-   await blogModel.findOneAndDelete(req.params.id);
-   return res.status(200).send({
-    success:true,
-    message:"Blog deleted successfully"
+    const blog = await blogModel
+    // .findOneAndDelete(req.params.id)
+    .findByIdAndDelete(req.params.id)
+    .populate("user");
+  await blog.user.blogs.pull(blog);
+  await blog.user.save();
+  return res.status(200).send({
+    success: true,
+    message: "Blog Deleted!",
+  });
 
-   })
+   
 
     
 } catch (error) {
