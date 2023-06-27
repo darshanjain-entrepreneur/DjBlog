dccc29f57020456d6322c5650376d5fd6c16c105
Darshan <darshanjain.godha@gmail.com>
2023-06-27 11:26:28 +0530

    added the redux state management

--- djBlog/client/src/components/Header.jsx
+++ djBlog/client/src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React , {useState} from 'react'
 import {Box , Button , Toolbar , AppBar, Typography, Tabs, Tab} from "@mui/material"
 import { Link } from 'react-router-dom'
-
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+
+
+const isLogin = useSelector(state => state.isLogin)
+console.log(isLogin);
+
 
 const [value , setValue] = useState()
 
@@ -14,19 +19,24 @@ const [value , setValue] = useState()
         <Typography variant='h4'>
            DjBlog
         </Typography>
-            <Box display={'flex'} margin-left={'auto'} marginRight={'auto'}>
-             <Tabs textColor='inherit'value={value} onChange={(e , val) => setValue(val)}>
-               <Tab label="Blogs" LinkComponent={Link} to="/blogs"/>
-               <Tab label="My Blogs" LinkComponent={Link} to="/myblogs"/>
-             </Tabs>
-
-            </Box>
-
+            {isLogin && (
+                <Box display={'flex'} margin-left={'auto'} marginRight={'auto'}>
+                <Tabs textColor='inherit'value={value} onChange={(e , val) => setValue(val)}>
+                  <Tab label="Blogs" LinkComponent={Link} to="/blogs"/>
+                  <Tab label="My Blogs" LinkComponent={Link} to="/myblogs"/>
+                </Tabs>
+   
+               </Box>
+            )}
 
         <Box display={'flex'} marginLeft='auto'>
-              <Button  sx={{margin:1 , color:"white"}}LinkComponent={Link}to="/login" >Login</Button>
+                {!isLogin && (<>
+                    <Button  sx={{margin:1 , color:"white"}}LinkComponent={Link}to="/login" >Login</Button>
               <Button  sx={{margin:1 , color:"white"}} LinkComponent={Link}to="/register">Register</Button>
-              <Button  sx={{margin:1 , color:"white"}}>Logout</Button>
+                </>)}
+        {isLogin && (<>
+            <Button  sx={{margin:1 , color:"white"}}>Logout</Button>
+        </>)}
         </Box>
      </Toolbar>
 
